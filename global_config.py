4ee55a1fa3868782da6ce9deaed0ef5578ec7f4d
Sayandip Dutta <sayandip199309@gmail.com>
2020-08-23 17:49:35 +0530

    Fix config path bug in global_config.

--- global_config.py
+++ global_config.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 import os
+from os.path import join as pjoin
 
 from googleapiclient import discovery
 from googleapiclient.http import MediaIoBaseDownload
@@ -18,18 +19,30 @@ parser.add_argument(
     type=str, 
     default='instructions.ini'
 )
+parser.add_argument(
+    "-s", "--setting", 
+	help="name of setting_file", 
+    type=str, 
+    default='settings.json'
+)
+
+
 args = parser.parse_args()
 
 instruction_set = []
+CWD = os.getcwd()
 ABSPATH = pathlib.Path(__file__).parent.absolute().as_posix()
-CONFIG = os.path.join(ABSPATH, args.config)
+os.chdir(ABSPATH)
+CONFIG_DIR = 'config'
+CONFIG = pjoin(ABSPATH, CONFIG_DIR, args.config)
+SETTING = pjoin(ABSPATH, CONFIG_DIR, args.setting)
 
 def __instructions(class_type, args, target, special):
     instruction = namedtuple('instruciton', 'method args target special')
     if target == '.':
         target = BASE_TARGET
     elif not os.path.isdir(target):
-        target = os.path.join(BASE_TARGET, target)
+        target = pjoin(BASE_TARGET, target)
     elif os.path.isdir(target):
         target = target
     else:
@@ -41,10 +54,10 @@ def __instructions(class_type, args, target, special):
 
     return instruction(class_type, args, target, special)
 
-with open('settings.json', 'r+') as setting_file, open(CONFIG) as configs:
+with open(SETTING, 'r+') as setting_file, open(CONFIG) as configs:
     settings = json.load(setting_file)
     # globals.update(settings)
-    if not os.path.isfile(os.path.join(ABSPATH,'storage.json')):
+    if not os.path.isfile(pjoin(ABSPATH, CONFIG_DIR, 'storage.json')):
     # if True:
         HOME = ABSPATH
         settings.update(HOME=ABSPATH)
@@ -52,7 +65,7 @@ with open('settings.json', 'r+') as setting_file, open(CONFIG) as configs:
         json.dump(settings, setting_file, indent='\t')
     locals().update(settings)
     storage_file, cred_file = map(
-        lambda x: os.path.join(ABSPATH, CRED_FILES.get(x)), 
+        lambda x: pjoin(ABSPATH, CONFIG_DIR, CRED_FILES.get(x)), 
         ['storage_file', 'cred_file']
         )
     store = file.Storage(storage_file)
@@ -69,4 +82,4 @@ with open('settings.json', 'r+') as setting_file, open(CONFIG) as configs:
             args = args.split('|')
             download_info = __instructions(*args)
             instruction_set.append(download_info)
-        
+os.chdir(CWD)
